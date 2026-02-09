58a74e5968d3fca483a26cc74bee91ade9888004
Eazdan Mostafa Rafin <eazdan.rafin@gmail.com>
2026-02-09 13:05:00 +0600

    updating api_path

--- expense-tracker-app/src/lib/api.ts
+++ expense-tracker-app/src/lib/api.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://expense-tracker-r3tn.onrender.com';
-
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 // Create axios instance
 const api = axios.create({
   baseURL: API_BASE_URL,
